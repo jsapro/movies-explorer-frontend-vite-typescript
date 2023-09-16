c80fd7a94e7ef4f3fc04e5b3fc60a7417fc7408b
jsapro <77.3.77@mail.ru>
2023-09-16 20:36:16 +0300

    add types to constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,11 @@
+import type { SavedMovieType } from './types';
+
 export const BEATFILMMOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const BASIC_MOVIES_URL = 'https://api.nomoreparties.co';
 // export const MAIN_BACKEND_URL = 'http://localhost:3000';
 export const MAIN_BACKEND_URL = 'https://api.viewport-jsapro.nomoreparties.co';
 
-export const filter = (movies, search, isShort) => {
+export const filter = (movies: SavedMovieType[], search: string, isShort: boolean) => {
   return movies.filter((movie) => {
     const matchedSearch =
       movie.nameRU.trim().toLowerCase().includes(search.trim().toLowerCase()) ||
